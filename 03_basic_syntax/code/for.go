d8b352068efe0a1547c111f44341aef446b19b4e
Tiskae <macbook@Tiskaes-MacBook-Pro.local>
2023-08-22 16:08:50 +0100

    exercise: for range loop

--- 03_basic_syntax/code/for.go
+++ 03_basic_syntax/code/for.go
@@ -2,33 +2,33 @@
 
 package main
 
-// import "fmt"
+import "fmt"
 
-// func main() {
+func main() {
 
-// 	// 	// ****************************
+	// ****************************
 
-// 	// i := 1
+	i := 1
 
-// 	for i := 1; i <= 100; i++ {
-// 		fmt.Println(i)
-// 	}
+	for i := 1; i <= 100; i++ {
+		fmt.Println(i)
+	}
 
-// 	// 	// ****************************
+	// ****************************
 
-// 	// i := 1
+	j := 1
 
-// 	// for i <= 100 {
-// 	// 	fmt.Println(i)
-// 	// 	// This will behave like a while loop
-// 	// 	i += 1
-// 	// }
+	for j <= 100 {
+		fmt.Println(i)
+		// This will behave like a while loop
+		j += 1
+	}
 
-// 	// 	// ****************************
+	// 	// ****************************
 
-// 	// var mySentence = "This is a sentence"
+	var mySentence string = "This is a sentence"
 
-// 	// for index, letter := range mySentence {
-// 	// 	fmt.Println("Index:", index, "Letter:", letter)
-// 	// }
-// }
+	for index, value := range mySentence {
+		fmt.Printf("Index: %d, Value: %s\n", index, string(value))
+	}
+}
